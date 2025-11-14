2bc279c6860654f9cfe5d80b452c96cda2aabbef
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-11-14 19:41:30 +0100

    add mongoose

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
-const routes = require('./routes/user.routes');
-const logger = require('./middleware/logger');
+const routes = require("./routes/user.routes");
+const logger = require("./middleware/logger");
 
 app.use(express.json());
 
-app.use(logger)
+app.use(logger);
 
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
-    res.send("Hiiiii");
-})
+  res.send("Hiiiii");
+});
 
-app.listen(PORT, () => console.log(`Running on Port ${PORT}`))
+console.log("Database URL:", process.env.DATABASE_URL);
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to DB", err);
+  });
+
+app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
